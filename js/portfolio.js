97cd6a23315400dd7abf486dc4ad314c7f22c637
Yuko Sugiyama <sugi_yuko@hotmail.com>
2014-09-09 14:26:31 +1000

    add css effect

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+//here should be call the function with $(document).ready
+
+// var app = {};
+
+// app.Router = Backbone.Router.extend({
+//   routes: {
+//     '': 'index',
+//     'posts/:id': 'getPost'
+//   },
+
+// });
